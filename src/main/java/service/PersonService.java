828ec855aad015433bbe9be6d53acabcf7e60ff5
Vasu <vasu@pop-os.localdomain>
2020-04-17 22:28:43 -0700

    Update formatting

--- src/main/java/service/PersonService.java
+++ src/main/java/service/PersonService.java
@@ -7,11 +7,12 @@ import java.util.Collection;
 
 public class PersonService {
 
-    PersonService(){}
+  PersonService() {}
 
-    ImmutableSetMultimap<Integer, Person> groupByAge(Collection<Person> persons) {
-        final ImmutableSetMultimap.Builder<Integer, Person> personsByAge = ImmutableSetMultimap.builder();
-        persons.forEach(person -> personsByAge.put(person.age(), person));
-        return personsByAge.build();
-    }
+  ImmutableSetMultimap<Integer, Person> groupByAge(Collection<Person> persons) {
+    final ImmutableSetMultimap.Builder<Integer, Person> personsByAge =
+        ImmutableSetMultimap.builder();
+    persons.forEach(person -> personsByAge.put(person.age(), person));
+    return personsByAge.build();
+  }
 }
